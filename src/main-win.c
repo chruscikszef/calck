8a30d6937dad8dd7122825b15f55cf9c02b9838b
Chruscik <edgemicrosoft58@gmail.com>
2025-04-09 19:34:21 +0200

    Add files via upload

--- src/main-win.c
+++ src/main-win.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+#include <wchar.h>
 
 int main() {
   printf("Kalkulator kurwa v1.2\n");
@@ -10,25 +12,28 @@ int main() {
     char op, *lang = getenv("LANG");
     int pl = 0;
 
-    if (lang && strncmp(lang, "pl_PL", 5) == 0) {
-      pl = 1;
+    WCHAR localeName[LOCALE_NAME_MAX_LENGTH];
+    if (GetUserDefaultLocaleName(localeName, LOCALE_NAME_MAX_LENGTH)) {
+        if (wcsncmp(localeName, L"pl-PL", 5) == 0) {
+            pl = 1;
+        }
     }
 
-    if (pl == 1) {
+    if (pl) {
       printf("Podaj pierwszą liczbę: \n");
     } else {
         printf("Give first number: \n");
     }
     scanf("%f", &a);
 
-    if (pl == 1) {
+    if (pl) {
       printf("Podaj operator do obliczenia wyniku liczby (+, -, *, /): \n");
     } else {
         printf("Enter operator (+, -, *, /): \n");
     }
     scanf(" %c", &op);
 
-    if (pl == 1) {
+    if (pl) {
       printf("Podaj drugą liczbę: ");
     } else {
         printf("Give second number: \n");
@@ -52,8 +57,8 @@ int main() {
         if (b != 0) {
           result =  a / b;
         } else {
-            if (pl == 1) {
-              printf("Nie da się dzielić przez zero");
+            if (pl) {
+              printf("Nie da się dzielić przez zero\n");
             } else {
                 printf("Can't divide by zero\n");
               }
@@ -61,7 +66,7 @@ int main() {
         continue;
 
       default:    
-        if (pl == 1) {
+        if (pl) {
           printf("Nieznany operator: %c \n", op);
         } else {
             printf("Unknown operator %c \n", op);
@@ -69,7 +74,7 @@ int main() {
         continue;
     }
 
-    if (pl == 1) {
+    if (pl) {
       printf("Wynik: %f \n", result);
     } else {
         printf("Result: %f \n", result);
